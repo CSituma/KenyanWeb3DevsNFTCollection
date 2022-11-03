be45ef81f4d6c49829a83b5b28304d81dbadd585
CSituma <cnsituma@gmail.com>
2022-11-03 21:56:55 +0300

    first commit

---
+++ Server/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x878bfe54478a062d563ED49aF4819760650eFEb9";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
